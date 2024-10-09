53b367efff4cbb077153639a7175f7d383787ec5
unknown <hoangquan14102k2@gmail.com>
2024-10-09 08:38:37 +0700

    add 4 DTOs

---
+++ EventCheckinSystem.Repo/DTOs/EventDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventCheckinSystem.Repo.DTOs
+{
+    public class EventDTO
+    {
+        public int EventID { get; set; }
+        public string Name { get; set; }
+        public int OrganizationID { get; set; }
+        public string OrganizationName { get; set; }
+        public List<GuestGroupDTO> GuestGroups { get; set; }
+        public List<UserEventDTO> UserEvents { get; set; }
+    }
+
+    public class GuestGroupDTO
+    {
+        public int GuestGroupID { get; set; }
+        public string GroupName { get; set; }
+    }
+
+    
+}
+
